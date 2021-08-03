11dc4494b98e85eb33dfd204c64020b9e3bb3349
agonzalezlevio <agonzalezlevio.dev@gmail.com>
2021-08-03 02:38:43 -0400

    Actualización rest controller Customer

--- src/main/java/cl/app/controller/CustomerController.java
+++ src/main/java/cl/app/controller/CustomerController.java
@@ -33,90 +33,89 @@ import cl.app.service.ICustomerService;
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
-	
+
 	@Autowired
 	private ICustomerService customerService;
-	
+
 	@GetMapping("/customer")
 	public List<Customer> index() {
 		return this.customerService.findAll();
 	}
-	
+
 	@GetMapping("/customer/page/{page}")
 	public Page<Customer> index(@PathVariable Integer page) {
 		Pageable pageable = PageRequest.of(page, 4);
 		return this.customerService.findAll(pageable);
 	}
-	
+
 	@GetMapping("/customer/{id}")
 	public ResponseEntity<?> show(@PathVariable Long id) {
-		
+
 		Customer customer = null;
 		Map<String, Object> response = new HashMap<>();
-		
+
 		try {
 			customer = this.customerService.findById(id);
-		} catch(DataAccessException e) {
+		} catch (DataAccessException e) {
 			response.put("message", "Error when querying the database");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
-		if(customer == null) {
+
+		if (customer == null) {
 			response.put("message", "Customer ID '".concat(id.toString().concat("' does not exist in the database")));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
 		}
-		
+
 		return new ResponseEntity<Customer>(customer, HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/customer")
 	public ResponseEntity<?> create(@Valid @RequestBody Customer customer, BindingResult result) {
-		
+
 		Customer newCustomer = null;
 		Map<String, Object> response = new HashMap<>();
-		
-		if(result.hasErrors()) {
-			List<String> errors = result.getFieldErrors()
-					.stream()
-					.map(err -> "The field '" + err.getField() +"' "+ err.getDefaultMessage())
+
+		if (result.hasErrors()) {
+			List<String> errors = result.getFieldErrors().stream()
+					.map(err -> "The field '" + err.getField() + "' " + err.getDefaultMessage())
 					.collect(Collectors.toList());
-			
+
 			response.put("errors", errors);
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
 		}
-		
+
 		try {
 			newCustomer = this.customerService.save(customer);
-		} catch(DataAccessException e) {
+		} catch (DataAccessException e) {
 			response.put("message", "Error when querying the database");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
+
 		response.put("message", "The customer has been successfully created");
 		response.put("customer", newCustomer);
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 	}
-	
+
 	@PutMapping("/customer/{id}")
-	public ResponseEntity<?> update(@Valid @RequestBody Customer customer, BindingResult result, @PathVariable Long id) {
+	public ResponseEntity<?> update(@Valid @RequestBody Customer customer, BindingResult result,
+			@PathVariable Long id) {
 
 		Customer currentCustomer = this.customerService.findById(id);
 		Customer updatedCustomer = null;
 
 		Map<String, Object> response = new HashMap<>();
 
-		if(result.hasErrors()) {
-			List<String> errors = result.getFieldErrors()
-					.stream()
-					.map(err -> "The field '" + err.getField() +"' "+ err.getDefaultMessage())
+		if (result.hasErrors()) {
+			List<String> errors = result.getFieldErrors().stream()
+					.map(err -> "The field '" + err.getField() + "' " + err.getDefaultMessage())
 					.collect(Collectors.toList());
-			
+
 			response.put("errors", errors);
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
 		}
-		
+
 		if (currentCustomer == null) {
 			response.put("message", "Customer ID '".concat(id.toString().concat("' does not exist in the database")));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
@@ -128,7 +127,7 @@ public class CustomerController {
 			currentCustomer.setEmail(customer.getEmail());
 			currentCustomer.setCreateAt(customer.getCreateAt());
 			currentCustomer.setUpdateAt(new Date());
-			
+
 			updatedCustomer = this.customerService.save(currentCustomer);
 		} catch (DataAccessException e) {
 			response.put("message", "Error when updating customer from database");
@@ -141,22 +140,22 @@ public class CustomerController {
 
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 	}
-	
+
 	@DeleteMapping("/customer/{id}")
 	public ResponseEntity<?> delete(@PathVariable Long id) {
-		
+
 		Map<String, Object> response = new HashMap<>();
-		
+
 		try {
-		    this.customerService.delete(id);
+			this.customerService.delete(id);
 		} catch (DataAccessException e) {
 			response.put("message", "Error deleting customer from database");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
+
 		response.put("message", "Customer has been successfully removed");
-		
+
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
 	}
 
