56f22827c993a599d5a46ad868e6c6758a14bcd2
agonzalezlevio <agonzalezlevio.dev@gmail.com>
2021-08-03 01:27:24 -0400

    Creación datos de prueba

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO customers (name, lastname, email, create_at, update_at) VALUES('Mr. John', 'Doe', 'john.doe@email.com', '2018-01-02', '2018-01-02');
+INSERT INTO customers (name, lastname, email, create_at, update_at) VALUES('Linus', 'Torvalds', 'linus.torvalds@email.com', '2018-01-03', '2018-01-03');
+INSERT INTO customers (name, lastname, email, create_at, update_at) VALUES('Rasmus', 'Lerdorf', 'rasmus.lerdorf@email.com', '2018-01-04', '2018-01-04');
+INSERT INTO customers (name, lastname, email, create_at, update_at) VALUES('Erich', 'Gamma', 'erich.gamma@email.com', '2018-02-01', '2018-02-01');
+INSERT INTO customers (name, lastname, email, create_at, update_at) VALUES('Richard', 'Helm', 'richard.helm@email.com', '2018-02-10', '2018-02-10');
+INSERT INTO customers (name, lastname, email, create_at, update_at) VALUES('Ralph', 'Johnson', 'ralph.johnson@email.com', '2018-02-18', '2018-02-18');
+INSERT INTO customers (name, lastname, email, create_at, update_at) VALUES('John', 'Vlissides', 'john.vlissides@email.com', '2018-02-28', '2018-02-28');
+INSERT INTO customers (name, lastname, email, create_at, update_at) VALUES('Dr. James', 'Gosling', 'james.gosling@email.com', '2018-03-03', '2018-03-03');
+INSERT INTO customers (name, lastname, email, create_at, update_at) VALUES('Magma', 'Lee', 'magma.lee@email.com', '2018-03-04', '2018-03-04');
+INSERT INTO customers (name, lastname, email, create_at, update_at) VALUES('Tornado', 'Roe', 'tornado.roe@email.com', '2018-03-05', '2018-03-05');
+INSERT INTO customers (name, lastname, email, create_at, update_at) VALUES('Jade', 'Doe', 'jane.doe@email.com', '2018-03-06', '2018-03-06');
