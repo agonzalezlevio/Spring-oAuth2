01dfebf91486cc9fdcf5aa0f77fd71dbd768ba56
agonzalezlevio <agonzalezlevio.dev@gmail.com>
2021-08-04 03:51:06 -0400

    Implementación de protección de rutas en controller Customer

--- src/main/java/cl/app/controller/CustomerController.java
+++ src/main/java/cl/app/controller/CustomerController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -48,6 +49,7 @@ public class CustomerController {
 		return this.customerService.findAll(pageable);
 	}
 
+	@Secured({"ROLE_ADMIN", "ROLE_USER"})
 	@GetMapping("/customer/{id}")
 	public ResponseEntity<?> show(@PathVariable Long id) {
 
@@ -98,6 +100,7 @@ public class CustomerController {
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 	}
 
+	@Secured("ROLE_ADMIN")
 	@PutMapping("/customer/{id}")
 	public ResponseEntity<?> update(@Valid @RequestBody Customer customer, BindingResult result,
 			@PathVariable Long id) {
@@ -141,6 +144,7 @@ public class CustomerController {
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 	}
 
+	@Secured("ROLE_ADMIN")
 	@DeleteMapping("/customer/{id}")
 	public ResponseEntity<?> delete(@PathVariable Long id) {
 
